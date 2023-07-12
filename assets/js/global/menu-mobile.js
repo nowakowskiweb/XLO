cd0c4888a9314c90fde6a6ba51c366bb52d785d8
Lucas <nowakowskilukasz.web@gmail.com>
2023-07-12 12:34:09 +0200

    Commit

--- assets/js/global/menu-mobile.js
+++ assets/js/global/menu-mobile.js
@@ -2,9 +2,9 @@ export class MenuMobile {
     buttonEventToggle;
     bodyHtml;
     navbarMenu;
+    backgroundImageSection;
     navbarMenuActive = 'header__navbar--active';
     scrollDisabled = 'scroll-disabled';
-
 
     constructor() {
         const initialElements = this.setInitialElements();
@@ -15,11 +15,19 @@ export class MenuMobile {
     setInitialElements() {
         this.buttonEventToggle = document.querySelector("[data-navbar-button]")
         this.navbarMenu = document.querySelector("[data-navbar-menu]")
-        return this.buttonEventToggle && this.navbarMenu;
+        this.backgroundImageSection = document.querySelector("[data-background-image]")
+
+        return this.buttonEventToggle && this.navbarMenu && this.backgroundImageSection;
     }
 
     registerListeners() {
-        this.buttonEventToggle.addEventListener('click', () => this.toggleMobileMenu())
+        this.buttonEventToggle.addEventListener('click', (event) => {
+            const target = event.target;
+            if (target.closest('[data-navbar-button]')) {
+                this.toggleMobileMenu();
+            }
+        });
+        window.addEventListener('scroll', (e) => this.changeMenuStyles(e))
     }
 
     toggleMobileMenu() {
@@ -29,6 +37,20 @@ export class MenuMobile {
         } else {
             this.navbarMenu.classList.add(this.navbarMenuActive);
             document.body.classList.add(this.scrollDisabled)
+        }
+    }
+
+    changeMenuStyles(e) {
+        const scrollPosition = document.documentElement.scrollTop;
+        const navbarMenuHeight = this.navbarMenu.getBoundingClientRect().height;
+        const backgroundImageHeight = this.backgroundImageSection.getBoundingClientRect().height;
+
+        if(scrollPosition + navbarMenuHeight > backgroundImageHeight) {
+            const backgroundHeight = Math.min(scrollPosition + navbarMenuHeight - backgroundImageHeight, 74);
+
+            this.navbarMenu.style.background = `linear-gradient(to top, red ${backgroundHeight}px, transparent ${backgroundHeight}px)`;
+        } else {
+            this.navbarMenu.style.background = ''
         }
     }
 }
